3740639c2d5f09983b583b535b1aa1dea0705de9
Dexter Young <129320268+TheTexta@users.noreply.github.com>
2025-08-01 16:49:28 -0300

    Add Node test for formatDate

---
+++ formatDate.js
@@ -0,0 +1,10 @@
+function formatDate(iso) {
+  const d = new Date(iso);
+  return d.toLocaleDateString(undefined, {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  });
+}
+
+module.exports = formatDate;
