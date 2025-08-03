dee0001d7e0d27e962a9d8af3e9bcadca18bb3e2
TheTexta <dextery777@gmail.com>
2025-08-03 00:53:40 -0400

    service worker changes

--- assets/js/sw-register.js
+++ assets/js/sw-register.js
@@ -1,7 +1,5 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/nepobabiesruntheunderground/sw.js', {
-      scope: '/nepobabiesruntheunderground/'
-    });
+    navigator.serviceWorker.register('./sw.js');
   });
 }
