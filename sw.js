3c649e26bd357fdcf5e0211ab2a5397eb67ab1af
TheTexta <dextery777@gmail.com>
2025-08-03 00:45:58 -0400

    added new favicons and updated service worker regi
    stration

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
 const CACHE_NAME = 'nbu-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/css/styles.css',
-  '/assets/js/parallax.js',
-  '/assets/js/tvstatic.js',
-  '/assets/js/sw-register.js',
-  '/assets/images/favicon.png'
+  './',
+  './index.html',
+  './assets/css/styles.css',
+  './assets/js/parallax.js',
+  './assets/js/tvstatic.js',
+  './assets/js/sw-register.js',
+  './assets/images/favicons/favicon.ico',
+  './assets/images/favicons/favicon-16x16.png',
+  './assets/images/favicons/favicon-32x32.png',
+  './assets/images/favicons/favicon-96x96.png',
+  './assets/images/favicons/apple-touch-icon.png',
+  './assets/images/favicons/android-chrome-192x192.png',
+  './assets/images/favicons/android-chrome-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
