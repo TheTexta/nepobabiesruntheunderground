f4120f5558ba04a624a8dda711300434dd85e813
TheTexta <dextery777@gmail.com>
2025-10-01 15:44:12 -0400

    sw update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js — v7 (no offline page)
-const VERSION = '7';
+// sw.js — v8 (no offline page)
+const VERSION = '8';
 const STATIC_CACHE = `static-v${VERSION}`;
 
 // Compute base path for local dev vs GitHub Pages
